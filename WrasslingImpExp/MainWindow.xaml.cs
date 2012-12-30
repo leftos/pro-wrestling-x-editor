2bc6cbb0647d51298dcb19a03e54766ea57156d6
Leftos <leftos@gmail.com>
2012-12-31 01:35:56 +0200

    v0.1 commit

--- WrasslingImpExp/MainWindow.xaml.cs
+++ WrasslingImpExp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -10,7 +11,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Microsoft.Win32;
 
 namespace WrasslingImpExp
@@ -22,6 +22,9 @@ namespace WrasslingImpExp
     {
         public static MainWindow mwInstance;
 
+        public static string DocsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        public static string FilesPath = DocsPath + @"\Pro Wrestling X Editor\";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -32,13 +35,13 @@ namespace WrasslingImpExp
         private void btnExportModels_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
-            ofd.Title = "Pick the models file, as in NOT PORN";
+            ofd.Title = "Pick the models file";
             ofd.ShowDialog();
 
             if (String.IsNullOrWhiteSpace(ofd.FileName))
                 return;
 
-            GraphicsImportExport.ExportAll(ofd.FileName, new char[] { 'x', 'o', 'f', ' ', '0', '3', '0', '3' }, 72, "PWXMDP.dat");
+            GraphicsImportExport.ExportAll(ofd.FileName, new char[] { 'x', 'o', 'f', ' ', '0', '3', '0', '3' }, 72, "PWXMDP.dat", "Models");
         }
 
         private void btnExportTextures_Click(object sender, RoutedEventArgs e)
@@ -50,7 +53,7 @@ namespace WrasslingImpExp
             if (String.IsNullOrWhiteSpace(ofd.FileName))
                 return;
 
-            GraphicsImportExport.ExportAll(ofd.FileName, new byte[] { 137, 80, 78, 71, 13, 10, 26, 10 }, 72, "PWXTX.dat");
+            GraphicsImportExport.ExportAll(ofd.FileName, new byte[] { 137, 80, 78, 71, 13, 10, 26, 10 }, 72, "PWXTX.dat", "Textures");
         }
 
         private void btnExportSound_Click(object sender, RoutedEventArgs e)
@@ -62,7 +65,112 @@ namespace WrasslingImpExp
             if (String.IsNullOrWhiteSpace(ofd.FileName))
                 return;
 
-            SoundImportExport.ExportAll(ofd.FileName);
+            SoundImportExport.ExportAll(ofd.FileName, "Sounds");
+        }
+
+        private void btnImportModels_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Pick one of the models you want to import";
+            ofd.ShowDialog();
+
+            if (String.IsNullOrWhiteSpace(ofd.FileName))
+                return;
+
+            var path = ofd.FileName.Replace(Path.GetFileName(ofd.FileName), "");
+
+            ofd = new OpenFileDialog();
+            ofd.Title = "Pick the parameters file";
+            ofd.ShowDialog();
+
+            if (String.IsNullOrWhiteSpace(ofd.FileName))
+                return;
+
+            var namesFileInput = ofd.FileName;
+
+            var datFile = FilesPath + @"\PWXMD.dat";
+            var namesFileOutput = FilesPath + @"\PWXMDP.dat";
+            /*
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "Give me a fucking name, talk MOTHERFUCKER";
+            sfd.ShowDialog();
+
+            if (String.IsNullOrWhiteSpace(sfd.FileName))
+                return;
+
+            var datFile = sfd.FileName;
+
+            sfd = new SaveFileDialog();
+            sfd.Title = "Give me the name of the parameters file, or I'm killing your wife";
+            sfd.ShowDialog();
+
+            if (String.IsNullOrWhiteSpace(sfd.FileName))
+                return;
+
+            var namesFile = sfd.FileName;
+            */
+
+            GraphicsImportExport.ImportAll(path, datFile, namesFileOutput, namesFileInput, 72);
+        }
+
+        private void btnImportTextues_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Pick one of the textures you want to import";
+            ofd.ShowDialog();
+
+            if (String.IsNullOrWhiteSpace(ofd.FileName))
+                return;
+
+            var path = ofd.FileName.Replace(Path.GetFileName(ofd.FileName), "");
+
+            ofd = new OpenFileDialog();
+            ofd.Title = "Pick the parameters file";
+            ofd.ShowDialog();
+
+            if (String.IsNullOrWhiteSpace(ofd.FileName))
+                return;
+
+            var namesFileInput = ofd.FileName;
+
+            var datFile = FilesPath + @"\PWXLEVTEX.dat";
+            var namesFileOutput = FilesPath + @"\PWXTX.dat";
+            /*
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "Give me a fucking name, talk MOTHERFUCKER";
+            sfd.ShowDialog();
+
+            if (String.IsNullOrWhiteSpace(sfd.FileName))
+                return;
+
+            var datFile = sfd.FileName;
+
+            sfd = new SaveFileDialog();
+            sfd.Title = "Give me the name of the parameters file, or I'm killing your wife";
+            sfd.ShowDialog();
+
+            if (String.IsNullOrWhiteSpace(sfd.FileName))
+                return;
+
+            var namesFile = sfd.FileName;
+            */
+
+            GraphicsImportExport.ImportAll(path, datFile, namesFileOutput, namesFileInput, 72);
+        }
+
+        private void btnImportSounds_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Pick one of the sounds you want to import";
+            ofd.ShowDialog();
+
+            if (String.IsNullOrWhiteSpace(ofd.FileName))
+                return;
+
+            var path = ofd.FileName.Replace(Path.GetFileName(ofd.FileName), "");
+            var datFile = FilesPath + @"\PWXSD.dat";
+
+            SoundImportExport.ImportAll(path, datFile);
         }
     }
 }
--- WrasslingImpExp/ProWrestlingXImportExport.cs
+++ WrasslingImpExp/ProWrestlingXImportExport.cs
@@ -10,23 +10,23 @@ namespace WrasslingImpExp
 {
     internal static class GraphicsImportExport
     {
-        private static string DocsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-        private static string FilesPath = DocsPath + @"\ProWrestlingXModels";
+        private static string DocsPath = MainWindow.DocsPath;
+        private static string FilesPath = MainWindow.FilesPath;
 
-        public static void ExportAll(string filename, char[] header, int namesLength, string namesFile)
+        public static void ExportAll(string filename, char[] header, int namesLength, string namesFile, string folderName)
         {
             byte[] array = new byte[header.Length];
             for (int i = 0; i < header.Length; i++)
             {
                 array[i] = (byte) header[i];
             }
-            ExportAll(filename, array, namesLength, namesFile);
+            ExportAll(filename, array, namesLength, namesFile, folderName);
         }
 
         private static List<string> GrabNames(string filename, int namesLength, string namesFile)
         {
             string directory = filename.Replace(Path.GetFileName(filename), "");
-            string pathToFile = directory + @"\" + namesFile;
+            string pathToFile = namesFile;
 
             List<string> listOfNames = new List<string>();
 
@@ -49,16 +49,99 @@ namespace WrasslingImpExp
             return listOfNames;
         }
 
-        public static void ExportAll(string filename, byte[] header, int namesLength, string namesFile)
+        public static void ImportAll(string pathToFiles, string datFile, string namesFileOutput, string namesFileInput, int namesLength)
         {
-            List<string> names = GrabNames(filename, namesLength, namesFile);
-            if (!Directory.Exists(FilesPath))
+            if (!Directory.Exists(pathToFiles))
             {
-                Directory.CreateDirectory(FilesPath);
+                MessageBox.Show("You beautiful you, that folder doesn't exist!");
+                return;
+            }
+
+            List<string> filesToImport = Directory.GetFiles(pathToFiles).ToList();
+            List<string> filesToImport_Names = new List<string>();
+            filesToImport.ForEach(f => filesToImport_Names.Add(Path.GetFileName(f)));
+            
+            List<string> names = GrabNames(namesFileInput, namesLength, namesFileInput);
+
+            List<byte> data = new List<byte>();
+            List<int> offsets = new List<int>();
+            offsets.Add(0);
+            List<int> lengths = new List<int>();
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                var name = names[i];
+                if (filesToImport_Names.Contains(name))
+                {
+                    data.AddRange(File.ReadAllBytes(pathToFiles + @"\" + name).ToList());
+                    lengths.Add(Convert.ToInt32(new FileInfo(pathToFiles + @"\" + name).Length));
+                    offsets.Add(offsets[i] + lengths[i]);
+                }
+            }
+            File.WriteAllBytes(datFile, data.ToArray());
+            offsets.RemoveAt(offsets.Count - 1);
+
+            List<string> hexOffsets = new List<string>();
+            List<string> hexLengths = new List<string>();
+            for (int i = 0; i < offsets.Count; i++)
+            {
+                hexOffsets.Add(String.Format("{0:X}", offsets[i]).PadLeft(8, '0'));
+                hexLengths.Add(String.Format("{0:X}", lengths[i]).PadLeft(8, '0'));
+            }
+
+            BinaryWriter bw = new BinaryWriter(File.OpenWrite(namesFileOutput));
+            for (int j = 0; j < names.Count; j++)
+            {
+                var name = names[j];
+                bw.Write(name.ToCharArray());
+                bw.Write((byte) 0);
+                int countOfFEs = namesLength - name.Length - 9;
+                for (int i = 0; i < countOfFEs; i++)
+                {
+                    bw.Write((byte) 254);
+                }
+                var hexOffset = ReverseByteOrder(hexOffsets[j]);
+                for (int i = 0; i <= 6; i += 2)
+                {
+                    bw.Write(Convert.ToByte(hexOffset.Substring(i, 2), 16));
+                }
+
+                var hexLength = ReverseByteOrder(hexLengths[j]);
+                for (int i = 0; i <= 6; i += 2)
+                {
+                    bw.Write(Convert.ToByte(hexLength.Substring(i, 2), 16));
+                }
+            }
+            bw.Close();
+
+            MessageBox.Show("The files are packed.");
+        }
+
+        private static string ReverseByteOrder(string hexString)
+        {
+            string b1 = hexString.Substring(0, 2);
+            string b2 = hexString.Substring(2, 2);
+            string b3 = hexString.Substring(4, 2);
+            string b4 = hexString.Substring(6, 2);
+            string result = b4 + b3 + b2 + b1;
+            return result;
+        }
+
+        public static void ExportAll(string filename, byte[] header, int namesLength, string namesFile, string folderName)
+        {
+            List<string> names = GrabNames(filename, namesLength, filename.Replace(Path.GetFileName(filename), "") + @"\" + namesFile);
+            var folder = FilesPath + @"\" + folderName;
+            if (!Directory.Exists(folder))
+            {
+                if (!Directory.Exists(FilesPath))
+                {
+                    Directory.CreateDirectory(FilesPath);
+                }
+                Directory.CreateDirectory(folder);
             }
             else
             {
-                Directory.GetFiles(FilesPath).ToList().ForEach(File.Delete);
+                Directory.GetFiles(folder).ToList().ForEach(File.Delete);
             }
 
             BackgroundWorker worker = new BackgroundWorker();
@@ -100,14 +183,14 @@ namespace WrasslingImpExp
                                      {
                                          data = br.ReadBytes(Convert.ToInt32(headerOffsets[i + 1] - headerOffsets[i]));
                                      }
-                                     BinaryWriter bw = new BinaryWriter(File.OpenWrite(FilesPath + @"\" + names[i]));
+                                     BinaryWriter bw = new BinaryWriter(File.OpenWrite(folder + @"\" + names[i]));
                                      bw.Write(data);
                                      bw.Close();
                                  }
 
                                  br.Close();
 
-                                 MessageBox.Show("Your tits are nice, and the models are in your My Documents folder.");
+                                 MessageBox.Show("The models are in your My Documents folder.");
                              };
 
             worker.ProgressChanged += OnProgressChanged;
@@ -130,16 +213,26 @@ namespace WrasslingImpExp
 
     internal static class SoundImportExport
     {
-        private static string DocsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-        private static string FilesPath = DocsPath + @"\ProWrestlingXModels";
+        private static string DocsPath = MainWindow.DocsPath;
+        private static string FilesPath = MainWindow.FilesPath;
 
-        public static void ExportAll(string filename)
+        public static void ExportAll(string filename, string folderName)
         {
-            if (!Directory.Exists(FilesPath))
+            var folder = FilesPath + @"\" + folderName;
+            if (!Directory.Exists(folder))
             {
-                Directory.CreateDirectory(FilesPath);
+                if (!Directory.Exists(FilesPath))
+                {
+                    Directory.CreateDirectory(FilesPath);
+                }
+                Directory.CreateDirectory(folder);
             }
-            char[] header = new char[] {'r', 'i', 'f', 'f'};
+            else
+            {
+                Directory.GetFiles(folder).ToList().ForEach(File.Delete);
+            }
+
+            char[] header = new char[] {'R', 'I', 'F', 'F'};
             BackgroundWorker worker = new BackgroundWorker();
             worker.WorkerReportsProgress = true;
 
@@ -172,18 +265,39 @@ namespace WrasslingImpExp
                                      {
                                          data = br.ReadBytes(Convert.ToInt32(headerOffsets[i + 1] - headerOffsets[i]));
                                      }
-                                     BinaryWriter bw = new BinaryWriter(File.OpenWrite(FilesPath + @"\" + i + ".wav"));
+                                     BinaryWriter bw = new BinaryWriter(File.OpenWrite(folder + @"\" + i.ToString().PadLeft(8, '0') + ".wav"));
                                      bw.Write(data);
                                      bw.Close();
                                  }
                                  br.Close();
 
-                                 MessageBox.Show("Your tits are nice, and the models are in your My Documents folder.");
+                                 MessageBox.Show("The models are in your My Documents folder.");
                              };
 
             worker.ProgressChanged += GraphicsImportExport.OnProgressChanged;
 
             worker.RunWorkerAsync();
+        }
+
+        public static void ImportAll(string pathToFiles, string datFile)
+        {
+            if (!Directory.Exists(pathToFiles))
+            {
+                MessageBox.Show("You beautiful you, that folder doesn't exist!");
+                return;
+            }
+
+            List<string> filesToImport = Directory.GetFiles(pathToFiles).ToList();
+            filesToImport.Sort();
+
+            List<byte> data = new List<byte>();
+            for (int i = 0; i < filesToImport.Count; i++)
+            {
+                data.AddRange(File.ReadAllBytes(filesToImport[i]).ToList());
+            }
+            File.WriteAllBytes(datFile, data.ToArray());
+
+            MessageBox.Show("The files are packed.");
         }
     }
 }
